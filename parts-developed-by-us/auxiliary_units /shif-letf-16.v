c94b0ee17ea2bfaa4a3a49923cc3f3ffdfbacd03
matheus <matheusaragaofs@gmail.com>
2025-06-25 22:19:16 -0300

    Refactor Shift_left16 module to ShiftLeft_16 with updated input/output names and added comments for clarity

--- parts-developed-by-us/auxiliary_units /shif-letf-16.v
+++ parts-developed-by-us/auxiliary_units /shif-letf-16.v
@@ -1,6 +1,11 @@
-module Shift_left16 (
-    input wire [31:0] data_in,
-    output wire [31:0] data_out
+module ShiftLeft_16 (
+    input wire [31:0] input_value,
+    output wire [31:0] shifted_result
 );
-    assign data_out = data_in << 16;
+
+// input_value  - O valor de entrada de 32 bits a ser deslocado
+// shifted_result - O resultado do valor de entrada deslocado 16 bits para a esquerda
+
+    assign shifted_result = input_value << 16;
+
 endmodule
